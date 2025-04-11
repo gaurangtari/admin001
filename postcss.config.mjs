5cfbd087c57fe35d91cca3babe197647303fb619
gaurangtari <gaurangtari@gmail.com>
2025-04-11 18:29:11 +0530

    eod_11/4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0a192f",
+        secondary: "#112240",
+        accent: "#64ffda",
+      },
+    },
+  },
 };
 
 export default config;
